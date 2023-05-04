de74cccc716291a048e85bdcf4f3effc78a97d40
Наталья Лягина <Vischnevoenastroenie@yandex.ru>
2023-05-04 17:57:16 +0400

    Добавляет неработающее бургерное меню с исправлением подключений

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
-import {Burger} from './modules/burger-menu';
+import {Burger} from './modules/burger-menu/burger-menu';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
